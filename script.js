d086d1991e77a3e60880168d3efcbadd5f7ca161
Klaurent Madhi <120852517+klaumadhi@users.noreply.github.com>
2023-06-19 19:27:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  }
+
+
+
